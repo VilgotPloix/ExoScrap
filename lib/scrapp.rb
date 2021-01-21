a3a5e3f90597d1143b5114b4e7e111eed4c6b2dd
gustave ploix <vilgotploix@gmail.com>
2021-01-21 18:20:44 +0100

    finalisation de l'exo 1

--- lib/scrapp.rb
+++ lib/scrapp.rb
@@ -1,20 +1,23 @@
 require 'nokogiri'
-require 'httparty'
+require 'open-uri'
 require 'byebug'
 
 def scrap
 	url = "https://coinmarketcap.com/all/views/all/"
-	pas_analyse = HTTParty.get(url)
-	analyse = Nokogiri::HTML(pas_analyse)
-	arr_name = analyse.xpath('//*[@class="cmc-table-row"]//td[3]/div').map{|element| element = element.text}
-	arr_value = analyse.xpath('//*[@class="cmc-table-row"]//td[5]/div').map{|element| element = element.text}
+	page = Nokogiri::HTML(open(url))
+
+
+	arr_name = page.xpath('//*[@class="cmc-table-row"]//td[3]/div').map{|element| element = element.text}
+	arr_value = page.xpath('//*[@class="cmc-table-row"]//td[5]/div').map{|element| element = element.text}
+
 
 	arr_final_value = arr_value.map
 
 	hash_final = Hash[arr_name.zip(arr_final_value.map)]
 
-	byebug
+	puts hash_final
 
 end
 
 scrap
+
